2a992c8bba502d7358ff6ff1789705731898c04e
Christian Schou <christian.schou@hotmail.com>
2022-07-04 14:46:01 +0200

    Create DTOs for Request and Response Abstraction

---
+++ MediatR_Demo/Domain/DTOs/Requests/Movie/CreateMovieRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR_Demo.Core.Enums;
+
+namespace MediatR_Demo.Domain.DTOs.Requests.Movie
+{
+    public class CreateMovieRequest
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public MovieGenre Genre { get; set; }
+        public int? Rating { get; set; }
+    }
+}
---
+++ MediatR_Demo/Domain/DTOs/Responses/Movie/CreateMovieDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MediatR_Demo.Domain.DTOs.Responses.Movie
+{
+    public class CreateMovieDto
+    {
+        public CreateMovieDto(long id)
+        {
+            Id = id;
+        }
+
+        public long Id { get; set; }
+    }
+}
