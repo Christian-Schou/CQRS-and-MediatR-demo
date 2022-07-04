8aea18fc49b097075c71037b9b373d98068b2901
Christian Schou <christian.schou@hotmail.com>
2022-07-04 12:30:13 +0200

    Create Core and Domain Models

---
+++ MediatR_Demo/Core/Enums/MovieGenre.cs
@@ -0,0 +1,15 @@
+﻿namespace MediatR_Demo.Core.Enums
+{
+    public enum MovieGenre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        Thriller,
+        Western
+    }
+}
